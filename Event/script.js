f0f1b7271d8d1d58c6c3f75c5d447a99d86f1613
SanginiShetty <sanginijshetty@gmail.com>
2024-08-06 20:53:19 +0530

    .

--- Event/script.js
+++ Event/script.js
@@ -1,5 +1,10 @@
-let btn = document.querySelector("#btn1");
-btn.onclick = () => {
+document.addEventListener('DOMContentLoaded', () => {
+    let btn1 = document.querySelector("#btn1");
+    
+    btn1.addEventListener("click", (event) => {
         console.log("btn1 was clicked");
-        
-    };
+        let a = 25;
+        a++;
+        console.log(a);
+    });
+});
